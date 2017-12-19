4dcfafea29b00713069b6fc9d99ecc54b8d76112
Ernie Hsiung <ernie@littleyellowdifferent.com>
2017-12-19 17:46:30 -0500

    Move .dev to .site because of weird SSL things

--- build-db.sh
+++ build-db.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 sed -e '
-s/\/\/wbu.wpengine.com/\/\/wbu.dev/g;
-s/wbu.wpengine.com/wbu.dev/g;
+s/\/\/wbu.wpengine.com/\/\/wbu.site/g;
+s/wbu.wpengine.com/wbu.site/g;
 s/\/nas\/content\/staging\/wbu/\/srv\/www\/wbu\/public_html/g;
-s/www.theevergrey.com/seattle.wbu.dev/g;
-s/theevergrey.com/seattle.wbu.dev/g;
-s/www.thenewtropic.com/miami.wbu.dev/g;
-s/thenewtropic.com/miami.wbu.dev/g
+s/www.theevergrey.com/seattle.wbu.site/g;
+s/theevergrey.com/seattle.wbu.site/g;
+s/www.thenewtropic.com/miami.wbu.site/g;
+s/thenewtropic.com/miami.wbu.site/g
 ' mysql.sql > dev_mysql.sql
