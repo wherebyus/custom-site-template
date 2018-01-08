74e013210651482767fa1399faa744643b752497
Ernie Hsiung <ernie@littleyellowdifferent.com>
2018-01-08 11:48:52 -0500

    build: Add scripts to add plugins

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -9,6 +9,13 @@ WP_TYPE=`get_config_value 'wp_type' "single"`
 DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
 DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
 
+FRODO_URL="https://github.com/wherebyus/frodo.git"
+NEWS_NAME="wbu-newsletters"
+NEWS_URL="https://github.com/wherebyus/wbu-newsletters.git"
+REFER_NAME="wbu-referrals"
+REFER_URL="https://github.com/wherebyus/wbu-referrals.git"
+
+
 # Make a database, if we don't already have one
 echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
@@ -22,7 +29,7 @@ touch ${VVV_PATH_TO_SITE}/log/access.log
 
 # Install and configure the latest stable version of WordPress
 if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-load.php" ]]; then
-    echo "Downloading WordPress..."
+  echo "Downloading WordPress..."
   noroot wp core download --version="${WP_VERSION}"
 fi
 
@@ -50,3 +57,50 @@ else
   cd ${VVV_PATH_TO_SITE}/public_html
   noroot wp core update --version="${WP_VERSION}"
 fi
+
+cd ${VVV_PATH_TO_SITE}
+# go to public_html, it must exist
+if [ ! -d public_html ]
+then
+  echo "Not in the web directory"
+else
+  echo "Attempting to populate content"
+  cd public_html
+  git init
+  git remote add origin ${FRODO_URL}
+  git fetch
+  git reset origin/master
+  git checkout -- .
+  npm install
+  composer install
+
+  cd ${VVV_PATH_TO_SITE}
+  cd public_html/wp-content/plugins
+  git clone ${NEWS_URL}
+  cd ${NEWS_NAME}
+  git init
+  git remote add origin ${NEWS_URL}
+  git fetch
+  git reset origin/master
+  git checkout -- .
+  npm install
+  composer install
+
+  cd ${VVV_PATH_TO_SITE}
+  cd public_html/wp-content/plugins
+  git clone ${REFER_URL}
+  cd ${REFER_NAME}
+  git init
+  git remote add origin ${REFER_URL}
+  git fetch
+  git reset origin/master
+  git checkout -- .
+  npm install
+  composer install
+fi
+
+# TO DO - check for presence of .sql,
+# replace live strings with dev strings, and then import
+# into vagrant. We get the .sql from my.wpengine.com
+# /installs/SITE-NAME/backup_points#production ->
+# partial backup -> Entire database.
